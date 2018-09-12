cbe26b0d0d85fa1006ddd523000de0ebb008df64
Jordan West <jordwest@gmail.com>
2018-09-12 18:38:38 +1000

    outputs nested blocks

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 
 /// Represents a segment of a line, potentially containing another block
+#[derive(Clone)]
 enum LineSegment {
     Content(String),
     Block(Block),
@@ -10,12 +11,19 @@ impl LineSegment {
     fn write_to(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             LineSegment::Content(s) => write!(f, "{}", s),
-            LineSegment::Block(b) => write!(f, "[Recurse block]"),
+            LineSegment::Block(b) => b.write_to(f),
         }
     }
 }
 
+impl<T: Into<String>> From<T> for LineSegment {
+    fn from(v: T) -> Self {
+        LineSegment::Content(v.into())
+    }
+}
+
 /// Represents a single line inside a block of text
+#[derive(Clone)]
 struct Line(Vec<LineSegment>);
 
 impl<T: Into<String>> From<T> for Line {
@@ -24,12 +32,16 @@ impl<T: Into<String>> From<T> for Line {
     }
 }
 
-impl fmt::Display for Line {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0[0])
+impl Line {
+    fn write_to(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for segment in &self.0 {
+            segment.write_to(f)?;
+        }
+        Ok(())
     }
 }
 
+#[derive(Clone)]
 struct Block(Vec<Line>);
 
 impl<T: Into<String>> From<T> for Block {
@@ -40,7 +52,21 @@ impl<T: Into<String>> From<T> for Block {
 
 impl fmt::Display for Block {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0[0].0[0])
+        self.write_to(f)
+    }
+}
+
+impl Block {
+    fn write_to(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut first_line = true;
+        for line in &self.0 {
+            if !first_line {
+                write!(f, "\n");
+            }
+            first_line = false;
+            line.write_to(f)?;
+        }
+        Ok(())
     }
 }
 
@@ -50,8 +76,25 @@ mod tests {
 
     #[test]
     fn outputs_a_block() {
-        let result = GrammarParser::parse(Rule::file, TEST_TEMPLATE);
-        let file: File = result.unwrap().next().unwrap().into();
-        assert_eq!(file.templates[0].name, "template1");
+        use std::fmt::Write;
+        let one_line_block = Block(vec![Line(vec![LineSegment::from("body();")])]);
+        let block = Block(vec![
+            Line(vec![LineSegment::from("function test() {")]),
+            Line(vec![
+                LineSegment::from("  "),
+                LineSegment::Block(one_line_block.clone()),
+            ]),
+            Line(vec![LineSegment::from("}")]),
+        ]);
+        let mut s = String::new();
+
+        // block.write_to(&mut s);
+        write!(&mut s, "{}", block);
+
+        assert_eq!(s, "function test() {\n  body();\n}");
+
+        // let result = GrammarParser::parse(Rule::file, TEST_TEMPLATE);
+        // let file: File = result.unwrap().next().unwrap().into();
+        // assert_eq!(file.templates[0].name, "template1");
     }
 }
