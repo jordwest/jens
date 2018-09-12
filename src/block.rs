435a514a38779a9942ebff49440d9283a3176826
Jordan West <jordwest@gmail.com>
2018-09-12 19:10:05 +1000

    outputs indentation

--- src/block.rs
+++ src/block.rs
@@ -8,10 +8,10 @@ enum LineSegment {
 }
 
 impl LineSegment {
-    fn write_to(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn write_to(&self, f: &mut fmt::Formatter, prefix: &str) -> fmt::Result {
         match self {
             LineSegment::Content(s) => write!(f, "{}", s),
-            LineSegment::Block(b) => b.write_to(f),
+            LineSegment::Block(b) => b.write_to(f, prefix),
         }
     }
 }
@@ -33,9 +33,14 @@ impl<T: Into<String>> From<T> for Line {
 }
 
 impl Line {
-    fn write_to(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn write_to(&self, f: &mut fmt::Formatter, prefix: &str) -> fmt::Result {
+        let mut sub_prefix = String::from(prefix);
         for segment in &self.0 {
-            segment.write_to(f)?;
+            match segment {
+                LineSegment::Content(x) => sub_prefix = sub_prefix + x,
+                _ => (),
+            }
+            segment.write_to(f, &sub_prefix)?;
         }
         Ok(())
     }
@@ -52,19 +57,19 @@ impl<T: Into<String>> From<T> for Block {
 
 impl fmt::Display for Block {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.write_to(f)
+        self.write_to(f, "")
     }
 }
 
 impl Block {
-    fn write_to(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn write_to(&self, f: &mut fmt::Formatter, prefix: &str) -> fmt::Result {
         let mut first_line = true;
         for line in &self.0 {
             if !first_line {
-                write!(f, "\n");
+                write!(f, "\n{}", prefix);
             }
             first_line = false;
-            line.write_to(f)?;
+            line.write_to(f, prefix)?;
         }
         Ok(())
     }
@@ -78,23 +83,30 @@ mod tests {
     fn outputs_a_block() {
         use std::fmt::Write;
         let one_line_block = Block(vec![Line(vec![LineSegment::from("body();")])]);
-        let block = Block(vec![
-            Line(vec![LineSegment::from("function test() {")]),
-            Line(vec![
-                LineSegment::from("  "),
-                LineSegment::Block(one_line_block.clone()),
-            ]),
-            Line(vec![LineSegment::from("}")]),
+        let multi_line_block = Block(vec![
+            Line(vec![LineSegment::from("body();")]),
+            Line(vec![LineSegment::from("body2();")]),
         ]);
-        let mut s = String::new();
 
-        // block.write_to(&mut s);
-        write!(&mut s, "{}", block);
+        let make_function_block = |function_body: &Block| {
+            Block(vec![
+                Line(vec![LineSegment::from("function test() {")]),
+                Line(vec![
+                    LineSegment::from("  "),
+                    LineSegment::Block(function_body.clone()),
+                ]),
+                Line(vec![LineSegment::from("}")]),
+            ])
+        };
 
-        assert_eq!(s, "function test() {\n  body();\n}");
+        let one_func = make_function_block(&one_line_block);
+        let mut s1 = String::new();
+        write!(&mut s1, "{}", one_func);
+        assert_eq!(s1, "function test() {\n  body();\n}");
 
-        // let result = GrammarParser::parse(Rule::file, TEST_TEMPLATE);
-        // let file: File = result.unwrap().next().unwrap().into();
-        // assert_eq!(file.templates[0].name, "template1");
+        let two_funcs = make_function_block(&multi_line_block);
+        let mut s2 = String::new();
+        write!(&mut s2, "{}", two_funcs);
+        assert_eq!(s2, "function test() {\n  body();\n  body2();\n}");
     }
 }
