74e72f7df4fce4ccb09fa8f526412bf5aca84205
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-10-28 11:43:16 +1100

    Reexport Block enums

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-pub use block::{Block, Line};
+pub use block::{Block, IteratorLocation, Line, LineSegment};
 pub use grammar::File;
 
 mod block;
